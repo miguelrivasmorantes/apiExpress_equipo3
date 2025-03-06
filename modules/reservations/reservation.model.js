5eccba4e1fc4345de82ea312e2d530db50a244eb
miguelrivasmorantes <miguelrivasmorantes@gmail.com>
2025-03-07 00:31:54 +0100

    feature/create reservation model

---
+++ modules/reservations/reservation.model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const User = require('../users/user.model');
+const Hotel = require('../hotels/hotel.model');
+const Room = require('../rooms/room.model');
+
+const ReservationSchema = new Schema({
+    usuario_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    hotel_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Hotel',
+        required: true
+    },
+    habitacion_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'Room',
+        required: true
+    },
+    fecha_inicio: {
+        type: Date,
+        required: true
+    },
+    fecha_fin: {
+        type: Date,
+        required: true
+    },
+    precio_total: {
+        type: Number,
+        required: true
+    },
+    estado: {
+        type: String,
+        enum: ['confirmada', 'pendiente', 'cancelada'],
+        required: true
+    }
+}, {
+    timestamps: true,
+    versionKey: true
+});
+
+module.exports = mongoose.model('Reservation', ReservationSchema);
