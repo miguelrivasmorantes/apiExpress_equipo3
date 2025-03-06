5b6a9f604a61c87273c12c6e491b99791567fbcf
carlos-alvarado-25 <carlos.alvarado@alumnos.unetlantico.es>
2025-03-06 22:36:18 +0100

    fix: fixing development environment & creating API directories

--- routes/api.js
+++ routes/api.js
@@ -1,28 +1,18 @@
 const express = require("express");
 const router = express.Router();
 
-const holaMundoRouter = require("./api/holaMundo");
-router.use("/", holaMundoRouter);
-
-
 //Rutas para administrar hoteles
 
 //Rutas para administrar habitaciones
 
 //Rutas para administrar usuarios
 
-const usuarioRouter = require("./api/usuarios");
-router.use("/usuarios", usuarioRouter);
-
-const userRouter = require('../modules/user/user.module')().UserController;
-router.use("/users", userRouter);
+const userRouter = require("../modules/users/user.module")().UserController;
+router.use("/usuarios", userRouter);
 
 //Rutas para administrar reservas
 
 //Rutas para administrar pagos
-
-const pagoRouter = require("./api/pagos");
-router.use("/pagos", pagoRouter);
 
 //Rutas para administrar reseñas
 
